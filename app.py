c98f309d2140f189d13f929eb10d450f4b108d7e
Shivanshu Srivastava <shivanshu@trgtdigital.com>
2020-03-01 01:52:55 +0530

    alpha commit

--- app.py
+++ app.py
@@ -1,38 +1,64 @@
+import os
+import re
 from flask import Flask, render_template, request
 import urllib.request, json, random
-import os
 
 folder_name = "temp_folder"
+regex = re.compile('[@_!#$%^&*()<>?/\|}{~:]')
 
 app = Flask(__name__)
 
 @app.route('/', methods=['GET','POST'])
 def index():
-    solution_var = 'bbb'
-    if request.method == 'GET':
+
+    status_style = "style=display:none"
+    placeholder_parm = '...'
+    
+    if request.method == 'GET':    
         
         with urllib.request.urlopen("https://www.randomlists.com/data/words.json") as url:
             word_dict = json.loads(url.read().decode())['data']
-        
-        rand_value = random.randrange(1, len(word_dict))
-        solution_form = word_dict[rand_value]
-        open(folder_name+"/"+solution_form, "w")
+
+        '''
+        from nltk.corpus import words
+        word_list = words.words()
+        '''
 
         while True:
-            puzzle = ''.join(random.sample(solution_form, len(solution_form)))
-            if puzzle != solution_form:
+            rand_value = random.randrange(1, len(word_dict))
+            solution = word_dict[rand_value]
+            puzzle = ''.join(random.sample(solution, len(solution)))
+            if puzzle != solution and 8 > len(solution) > 3 and regex.search(puzzle) == None:
                 break
+
+        files_to_remove = [os.path.join(folder_name, f) for f in os.listdir(folder_name)]
+        for f in files_to_remove:
+            os.remove(f)
+            
+        open(folder_name+"/"+solution.lower()+'_'+puzzle.lower(), "w")
         
-        return render_template('index.html', puzzle=puzzle)
+        return render_template('index.html', solution=solution, puzzle=puzzle, status_style=status_style, placeholder_parm=placeholder_parm)
     
     elif request.method == 'POST':
+        
+        try:
+            filename = os.listdir(folder_name)[0]
+        except IndexError:
+            return render_template('index.html')
 
-        filename = os.listdir(folder_name)[0]
-        filesize = os.path.getsize(folder_name+'/'+filename)
-        if filesize == 0:
-            os.remove(folder_name+'/'+filename)
+        answer = request.form['answer'].lower()
+        solution, puzzle = filename.partition('_')[0], filename.partition('_')[2]
+        
+        if answer == solution:
+            status = "id=fa-check"
+            status_style = "style=border-width:2px;border-style:solid;background:limegreen;color:white;line-height:inherit"
+        else:
+            status = "id=fa-close"
+            status_style = "style=border-width:2px;border-style:solid;background:red;color:white;line-height:inherit"
 
-        return render_template('index.html', filename=filename)
+        placeholder_parm = ':' + answer
+            
+        return render_template('index.html', solution=solution, puzzle=puzzle, status=status, status_style=status_style, placeholder_parm=placeholder_parm)
 
 if __name__ == '__main__':
     app.run(debug=True)
